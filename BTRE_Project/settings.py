f2cb2673f86d9c9717125c17080af69764987654
balaji762995 <balaji761995@gmail.com>
2019-11-23 20:03:40 +0530

    'added_django-heroku'

--- BTRE_Project/settings.py
+++ BTRE_Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -156,3 +157,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'balaji761995@gmail.com'
 EMAIL_HOST_PASSWORD = 'Oct2019@'
 EMAIL_USE_TLS = True
+
+
+django_heroku.settings(local())
